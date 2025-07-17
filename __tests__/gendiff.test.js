5ce0c35a2145fe9feb32796c902e2465aded32ea
root <root@DESKTOP-NSO6VME.localdomain>
2025-07-17 19:18:21 +0500

    fixed

---
+++ __tests__/gendiff.test.js
@@ -0,0 +1,21 @@
+import { readFileSync } from 'fs';
+import path from 'path';
+import genDiff from '../src/index.js';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
+
+test.each([
+  ['file1.json', 'file2.json', 'stylish', 'expectedStylish.txt'],
+  ['file1.yaml', 'file2.yaml', 'plain', 'expectedPlain.txt'],
+  ['file1.json', 'file2.json', 'json', 'expectedJson.txt'],
+])('genDiff %s %s with format %s', (file1, file2, format, expectedFile) => {
+  const expected = readFile(expectedFile).trim();
+  const filepath1 = getFixturePath(file1);
+  const filepath2 = getFixturePath(file2);
+  expect(genDiff(filepath1, filepath2, format)).toBe(expected);
+});
---
+++ src/index.js
@@ -0,0 +1,41 @@
+import fs from 'fs';
+import path from 'path';
+import yaml from 'js-yaml';
+import _ from 'lodash';
+import buildDiff from './buildDiff.js';
+import getFormatter from './formatters/index.js';
+
+const parse = (data, format) => {
+  switch (format) {
+    case 'json':
+      return JSON.parse(data)
+    case 'yml':
+    case 'yaml':
+      return yaml.load(data)
+    default:
+      throw new Error(`Unsupported file extension: ${format}`)
+  }
+}
+
+const parseFile = (filepath1, filepath2) => {
+  const raw1 = fs.readFileSync(filepath1, 'utf-8')
+  const raw2 = fs.readFileSync(filepath2, 'utf-8')
+
+  const format1 = path.extname(filepath1).slice(1)
+  const format2 = path.extname(filepath2).slice(1)
+
+  const data1 = parse(raw1, format1)
+  const data2 = parse(raw2, format2)
+
+  return { data1, data2 }
+}
+
+const genDiff = (filepath1, filepath2, format = 'stylish') => {
+  const { data1, data2 } = parseFile(filepath1, filepath2)
+  const diffTree = buildDiff(data1, data2)
+
+  const formatDiff = getFormatter(format)
+  return formatDiff(diffTree)
+}
+
+export default genDiff
